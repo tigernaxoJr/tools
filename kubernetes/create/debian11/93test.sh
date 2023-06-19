362e3ed0880ae55f501fe577cc53dca1d31d8ca6
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-06-19 15:49:44 +0800

    tune

--- kubernetes/create/debian11/93test.sh
+++ kubernetes/create/debian11/93test.sh
@@ -12,9 +12,10 @@ apt install -y vim git sudo
 
 # test
 ./92taint-control-plane.sh
-kubectl apply -f ./yml/demo.yml
-kubectl apply -f ./yml/service.yml
+kubectl create -f ./yml/demo.yml
+kubectl create -f ./yml/service.yml
 
 # give kubectl privileges to the sudo user
 usermod -aG sudo ${SUDO_USER}
-su - ${SUDO_USER} -c "bash ./40kubectl-non-root.sh"
+cp ./40kubectl-non-root.sh /tmp/kubectl-non-root.sh 
+su - ${SUDO_USER} -c "bash /tmp/40kubectl-non-root.sh"
