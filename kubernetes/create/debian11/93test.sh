0f2802e8458ffc31892a1f5bf6af199bd7ef169d
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-06-19 15:50:08 +0800

    tune

--- kubernetes/create/debian11/93test.sh
+++ kubernetes/create/debian11/93test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-SUDO_USER="naxo"
 # todo ip table
 sed -i 's/^deb cdrom/# deb cdrom/' /etc/apt/sources.list
 chmod a+x ./*.sh
@@ -16,6 +15,7 @@ kubectl create -f ./yml/demo.yml
 kubectl create -f ./yml/service.yml
 
 # give kubectl privileges to the sudo user
+SUDO_USER="naxo"
 usermod -aG sudo ${SUDO_USER}
 cp ./40kubectl-non-root.sh /tmp/kubectl-non-root.sh 
 su - ${SUDO_USER} -c "bash /tmp/40kubectl-non-root.sh"
