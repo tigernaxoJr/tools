7f1955e879836e82566040cc4f29b827a5880796
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-06-19 17:07:22 +0800

    kubectl privileges script

--- kubernetes/create/debian11/40kubectl-config.sh
+++ kubernetes/create/debian11/40kubectl-config.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+if [[ -n "${1}" ]]; then
+  USER="${1}"
+  CONFIG_DIR=/home/${USER}/.kube
+else
+  USER="$(id -u)"
+  CONFIG_DIR=${HOME}/.kube
+fi
+if [[ -n "${2}" ]]; then
+  GROUP="${1}"
+else
+  GROUP="$(id -u)"
+fi
+mkdir -p ${CONFIG_DIR}
+cp -i /etc/kubernetes/admin.conf ${CONFIG_DIR}/config
+chown ${USER}:${GROUP} ${CONFIG_DIR}/config
--- kubernetes/create/debian11/93test.sh
+++ kubernetes/create/debian11/93test.sh
@@ -12,9 +12,7 @@ apt-get install -y vim git sudo
 # give kubectl privileges to the sudo user
 SUDO_USER="naxo"
 usermod -aG sudo ${SUDO_USER}
-cp ./41kubectl-config-non-root.sh /tmp/kubectl-config.sh 
-chmod a+s /tmp/kubectl-config.sh
-su - ${SUDO_USER} -c "bash /tmp/kubectl-config.sh"
+./40kubectl-config.sh ${SUDO_USER}
 
 # test
 ./92taint-control-plane.sh
