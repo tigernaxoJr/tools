b3270f370a6441958e5704d34f4daf90f76ef733
Yu-Cheng,Chen <tigernaxo@gmail.com>
2023-06-19 16:16:04 +0800

    try

--- kubernetes/create/debian11/93test.sh
+++ kubernetes/create/debian11/93test.sh
@@ -7,15 +7,16 @@ apt install -y vim git sudo
 
 ./91static-ip.sh enp0s8 192.168.56.150
 ./master.sh > ./log.sh
-./41kubectl-root.sh
+./40kubectl-config.sh
+
+# give kubectl privileges to the sudo user
+SUDO_USER="naxo"
+usermod -aG sudo ${SUDO_USER}
+cp ./40kubectl-config.sh /tmp/kubectl-config.sh 
+chmod a+s /tmp/kubectl-config.sh
+su - ${SUDO_USER} -c "bash /tmp/kubectl-config.sh"
 
 # test
 ./92taint-control-plane.sh
 kubectl create -f ./yml/demo.yml
 kubectl create -f ./yml/service.yml
-
-# give kubectl privileges to the sudo user
-SUDO_USER="naxo"
-usermod -aG sudo ${SUDO_USER}
-cp ./40kubectl-non-root.sh /tmp/kubectl-non-root.sh 
-su - ${SUDO_USER} -c "bash /tmp/40kubectl-non-root.sh"
